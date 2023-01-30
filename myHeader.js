d18dfea6dd23320986d75800c8556c6d2d05ab5c
lucassaureliano <lucassaureliano@gmail.com>
2023-01-30 19:22:17 -0300

    init

---
+++ myHeader.js
@@ -0,0 +1,22 @@
+class MyHeader extends HTMLElement{
+    connectedCallback() {
+        //createElement = cria uma elemento HTML baseado numa TAG especificada
+        const header = document.createElement("header");
+        const nav = document.createElement("nav");
+        const ul = document.createElement("ul");
+
+        for(let i = 0; i < 5; i++){
+            let li = document.createElement("li");
+            li.textContent = `Pagina ${i}`;
+            ul.appendChild(li);
+        }
+
+        nav.appendChild(ul);
+        header.appendChild(nav);
+        this.appendChild(header);
+
+    }
+
+}
+
+customElements.define("my-header", MyHeader);
